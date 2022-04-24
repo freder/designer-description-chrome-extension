a5748c8dd632055d7e7adbf43af40e5d1d29e72a
Frederic Brodbeck <der.froedi@gmail.com>
2022-04-24 19:04:17 +0200

    also display hardcoded list

--- options.js
+++ options.js
@@ -1,16 +1,8 @@
-const container = document.getElementById('list');
+/* global hostBlacklist, setBlocked, getBlockedFromStorage */
 
-const removeAllButton = document.getElementById('remove-all');
-removeAllButton.onclick = (event) => {
-	console.log('here');
-	event.preventDefault();
-	setBlocked([]).then(
-		() => window.location.reload()
-	);
-};
-
-getBlockedFromStorage().then((urls) => {
-	urls.forEach((url) => {
+function populateHardcodedList() {
+	const container = document.getElementById('list-hardcoded');
+	hostBlacklist.forEach((url) => {
 		const elem = document.createElement('div');
 
 		const link = document.createElement('a');
@@ -18,20 +10,50 @@ getBlockedFromStorage().then((urls) => {
 		link.href = 'http://' + url;
 		link.target = '_blank';
 
-		const button = document.createElement('a');
-		button.className = 'remove-btn';
-		button.textContent = 'remove';
-		button.onclick = (event) => {
-			event.preventDefault();
-			setBlocked(
-				urls.filter((u) => u !== url)
-			).then(
-				() => window.location.reload()
-			);
-		};
-
-		elem.appendChild(button);
 		elem.appendChild(link);
 		container.appendChild(elem);
 	});
-});
+}
+
+function populateList() {
+	const container = document.getElementById('list');
+
+	const removeAllButton = document.getElementById('remove-all');
+	removeAllButton.onclick = (event) => {
+		console.log('here');
+		event.preventDefault();
+		setBlocked([]).then(
+			() => window.location.reload()
+		);
+	};
+
+	getBlockedFromStorage().then((urls) => {
+		urls.forEach((url) => {
+			const elem = document.createElement('div');
+
+			const link = document.createElement('a');
+			link.textContent = url;
+			link.href = 'http://' + url;
+			link.target = '_blank';
+
+			const button = document.createElement('a');
+			button.className = 'remove-btn';
+			button.textContent = 'remove';
+			button.onclick = (event) => {
+				event.preventDefault();
+				setBlocked(
+					urls.filter((u) => u !== url)
+				).then(
+					() => window.location.reload()
+				);
+			};
+
+			elem.appendChild(button);
+			elem.appendChild(link);
+			container.appendChild(elem);
+		});
+	});
+}
+
+populateHardcodedList();
+populateList();
